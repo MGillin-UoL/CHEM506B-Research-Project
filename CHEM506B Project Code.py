d312e6f72cf886987b89e8f1a8f8a0eb8c0c809d
Michael Gillin <sgmgilli@liverpool.ac.uk>
2025-06-30 15:54:36 +0100

    Update CHEM506B Project Code.py

--- CHEM506B Project Code.py
+++ CHEM506B Project Code.py
@@ -62,6 +62,45 @@ MOVEMENT_PARAMS = {
 CSV_FILE_PATH = os.path.expanduser("~/Code/chem504-2425_GroupA/examples/blue_pixel_data.csv")
 
 
+
+# === IKA RCT Digial Hotplate Configuration
+
+import serial.tools.list_ports
+from PyLabware.devices.ika_rct_digital import RCTDigitalHotplate
+
+# Detects serial port
+def find_hotplate_port():
+    """Return the first likely IKA port e.g. /dev/ttyACM0."""
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        if port.device.startswith('/dev/ttyACM') or port.device.startswith('/dev/ttyUSB'):
+            print(f"Found device on {port.device}")
+            return port.device
+    raise IOError("No IKA hotplate serial port found! Please check connection.")
+
+serial_port = find_hotplate_port()
+
+# Required parameters
+device_name = "IKA RCT Digital"
+connection_mode = "serial"
+address = None
+
+# Create the hotplate instance
+plate = RCTDigitalHotplate(
+    device_name=device_name,
+    connection_mode=connection_mode,
+    address=address,
+    port=serial_port
+)
+
+# Establish the connection
+plate.connect()
+
+# Initialise/reset it
+plate.initialize_device()
+
+
+
 # === Helper Functions ===
 def degreestorad(angles_deg):
     return [angle * math.pi / 180 for angle in angles_deg]
@@ -90,6 +129,14 @@ def main():
     move_robot(robot, ROBOT_POSITIONS["Home_Position"])
     print("Starting in home position", "\n")
     
+    # Send commands
+    plate.set_temperature(50)
+    print(f"Temperature: {plate.get_temperature()} °C")
+
+    plate.set_speed(1500)
+    print(f"Speed: {plate.get_speed()} rpm")
+
+    plate.start_stirring()
 
     # Loading Mettler Toledo Quantos with Sample Vial 1
     operate_gripper(gripper, 0)
