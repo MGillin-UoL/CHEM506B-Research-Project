c80199a911b0c6da08756e6550ec7b28751ed91d
Michael Gillin <sgmgilli@liverpool.ac.uk>
2025-08-22 15:52:28 +0100

    Update Test_Gripper.py

--- Test_Gripper.py
+++ Test_Gripper.py
@@ -1,7 +1,7 @@
 
 from robotiq.robotiq_gripper import RobotiqGripper
 
-HOST = "192.168.0.2"
+HOST = "192.168.10.2"
 PORT = 30003
 
 def main():
@@ -10,10 +10,20 @@ def main():
     gripper=RobotiqGripper()
     gripper.connect(HOST, 63352)
     #gripper.activate()
-    gripper.move(255, 255, 255)
-    # First number: How far it opens (0 fully open) and closes (255 fully closed)
+    gripper.move(180, 255, 255)
+
+    # First number: How far it opens and closes
+    # - 0 = Fully open
+    # - 125 = Large sample vial main body (40 ml)
+    # - 140 = Normal sample vial with lid
+    # - 155 = Normal sample vial without lid
+    # - 180 = Between pipettes
+    # - 200 = Holding pipette
+    # - 240 = Dosing head
+    # - 255 = Fully closed
     # Second number: How quickly it opens and closes (0 slowest, 255 fastest)
     # Third number: Force (0 minimum, 255 maximum)
+
 
 if __name__ == '__main__':
     main()
