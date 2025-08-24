80fe368ac8872b73f7909cfc2c10e240cbc89191
Michael Gillin <sgmgilli@liverpool.ac.uk>
2025-08-24 13:53:11 +0100

    Update Test_Get_Joints.py

--- Test_Get_Joints.py
+++ Test_Get_Joints.py
@@ -1,8 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Frugal Automation for Crystallisation Analysis
+Webcam-Based Light Scattering for Particle Characterisation
 
+Author: Michael Gillin (University of Liverpool, Digital Chemistry MSc)
+Supervisors: Dr Joe Forth, Dr Gabriella Pizzuto
+Date: August 2025
+
+Description:
+This script enables the user to obtain the six-axis coordinates for a UR5e
+robotic arm running off URScript. Once ran, the coordinates in the terminal
+can be copied and pasted into the main robotic movements code.
+
+Notes:
+This script was developed as part of Michael Gillin's Digital Chemistry MSc
+dissertation at the University of Liverpool, "Frugal Automation for Crystallisation
+Analysis: Webcam-Based Light Scattering for Low-Cost Particle Characterisation"
+
+GitHub: https://github.com/MGillin-UoL
+Email: sgmgilli@liverpool.ac.uk
+"""
 
 import sys
 import os
-import time
 
 # Add the directory containing robotiq_preamble.py to the Python search path
 current_dir = os.path.dirname(os.path.abspath(__file__))
